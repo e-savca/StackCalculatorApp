9a2befb14df21d9177c64f4fbc5687a2c0b4aa27
Eugeniu Savca <eusavca@gmail.com>
2023-11-11 16:58:41 +0200

    some updates in TokenizeExpression method

--- StackCalculatorApp.Library/Extensions/Extensions.cs
+++ StackCalculatorApp.Library/Extensions/Extensions.cs
@@ -17,109 +17,65 @@ public static class Extensions
     public static bool IsValidCharacter(this char keyChar) => "0123456789/*-+()".Contains(keyChar);
 
     public static List<Token> TokenizeExpression(this string expression)
+{
+    List<Token> tokens = new List<Token>();
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < expression.Length; i++)
     {
-        List<Token> tokens = new List<Token>();
+        char c = expression[i];
 
-        // Split the expression into individual characters
-        char[] chars = expression.ToCharArray();
-        
-        // Iterate over each character and create a token for it
-        for (int i = 0; i < chars.Length; i++)
+        if (char.IsWhiteSpace(c))
         {
-            char c = chars[i];
-
-            if (char.IsDigit(c))
-            {
-                // If the character is a digit, parse the number and create a number token
-                StringBuilder sb = new StringBuilder();
-                sb.Append(c);
-
-                // Keep parsing digits until we reach the end of the number
-                //while (i + 1 < chars.Length && (char.IsDigit(chars[i + 1]) || chars[i + 1] == '.'))
-                while (i + 1 < chars.Length && char.IsDigit(chars[i + 1]))
-                {
-                    sb.Append(chars[i + 1]);
-                    i++;
-                }
-                tokens.Add(new Token(sb.ToString(), TokenType.Number));
-            }
-            else if ("+*/".Contains(c))
-            {
-                tokens.Add(new Token(c.ToString(), TokenType.Operator));
-            }
-            else if (c == '-')
-            {
-                // Check if the minus sign is a negative number or a subtraction operator
-                if (chars[i - 1].ToString().IsMathOperator() && i + 1 < chars.Length && char.IsDigit(chars[i + 1]))
-                {
-                    // If the next character is a digit, parse the negative number and create a number token
-                    StringBuilder sb = new StringBuilder();
-                    sb.Append(c);
-
-                    // Keep parsing digits until we reach the end of the number
-                    //while (i + 1 < chars.Length && (char.IsDigit(chars[i + 1]) || chars[i + 1] == '.'))
-                    while (i + 1 < chars.Length && char.IsDigit(chars[i + 1]))
-                    {
-                        sb.Append(chars[i + 1]);
-                        i++;
-                    }
-
-                    tokens.Add(new Token(sb.ToString(), TokenType.Number));
-                }
-                //else if ()
-                //{
-
-                //}
-                else
-                {
-                    tokens.Add(new Token(c.ToString(), TokenType.Operator));
-                }
-            }
-            else if (c == '*')
-            {
-                tokens.Add(new Token(c.ToString(), TokenType.Operator));
-            }
-            //else if (c == '/')
-            //{
-            //    // Check if the divide sign is before a negative number in parentheses
-            //    bool isNegative = false;
-            //    int j = i + 1;
-            //    while (j < chars.Length && chars[j] == ' ')
-            //    {
-            //        j++;
-            //    }
-            //    if (j < chars.Length && chars[j] == '-')
-            //    {
-            //        isNegative = true;
-            //    }
-
-            //    if (i > 0 && chars[i - 1] == ')' && isNegative)
-            //    {
-            //        // If the divide sign is before a negative number in parentheses, create a multiplication token instead
-            //        tokens.Add(new Token("*", TokenType.Operator));
-            //        tokens.Add(new Token("-1", TokenType.Number));
-
-            //        // Skip the negative sign and whitespace
-            //        i++;
-            //    }
-            //    else
-            //    {
-            //        tokens.Add(new Token(c.ToString(), TokenType.Operator));
-            //    }
-            //}
-            else if (c == '(')
-            {
-                tokens.Add(new Token(c.ToString(), TokenType.Parenthesis));
-            }
-            else if (c == ')')
-            {
-                tokens.Add(new Token(c.ToString(), TokenType.Parenthesis));
-            }
+            continue;
         }
 
-        return tokens;
+        if (char.IsDigit(c) || (c == '.' && i + 1 < expression.Length && char.IsDigit(expression[i + 1])))
+        {
+            sb.Clear();
+            do
+            {
+                sb.Append(c);
+                c = ++i < expression.Length ? expression[i] : '\0';
+            }
+            while (char.IsDigit(c) || c == '.');
+
+            tokens.Add(new Token(sb.ToString(), TokenType.Number));
+            i--;
+        }
+        else if (c == '-' && i + 1 < expression.Length && expression[i + 1] == '(')
+        {
+            tokens.Add(new Token("1", TokenType.Number));
+            tokens.Add(new Token("*", TokenType.Operator));
+            tokens.Add(new Token("-1", TokenType.Number));
+            i++;
+        }
+        else
+        {
+            TokenType type;
+            switch (c)
+            {
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                    type = TokenType.Operator;
+                    break;
+                case '(':
+                case ')':
+                    type = TokenType.Parenthesis;
+                    break;
+                default:
+                    throw new ArgumentException($"Invalid character in expression: {c}");
+            }
+            tokens.Add(new Token(c.ToString(), type));
+        }
     }
 
+    return tokens;
+}
+
+    
 
 
 }
